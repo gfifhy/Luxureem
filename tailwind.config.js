efcc84bd4d03cf253d8d4028df92b3969d48bd3d
DengOwO <odengmalinao@gmail.com>
2023-12-03 08:06:25 +0800

    hero section and product list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./frontend/**/*.{html,js}"],
+  content: [
+    "./frontend/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+     require('flowbite/plugin')
+    ],
 }
 
