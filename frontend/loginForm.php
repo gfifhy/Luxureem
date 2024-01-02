f5e6148c0dec130d9ca0d90b1537183508cae7f2
DengOwO <odengmalinao@gmail.com>
2024-01-02 13:17:31 +0800

    asd

--- frontend/loginForm.php
+++ frontend/loginForm.php
@@ -118,7 +118,7 @@
           Luxureem
         </a>
         <div
-          class="w-11/12 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+          class="w-4/5 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
         >
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1
--- frontend/userDashboard.php
+++ frontend/userDashboard.php
@@ -33,8 +33,7 @@
     <aside
       id="logo-sidebar"
       class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-      aria-label="Sidebar"
-    >
+      aria-label="Sidebar">
       <div
         class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 w-"
       >
