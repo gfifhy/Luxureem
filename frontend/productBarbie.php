0d07b0e8a22930507a3cc797ebcaec05e319d38a
BotRonin <charlestan320@gmail.com>
2024-01-03 20:09:30 +0800

    asd

--- frontend/productBarbie.php
+++ frontend/productBarbie.php
@@ -252,7 +252,7 @@ if ($conn->connect_error) {
           
 
         <?php
-$sql = "SELECT barbiename, barbiedesc, barbiepic FROM barbie";
+$sql = "SELECT barbiename, barbiedesc, barbiepic, barbieprice FROM barbie";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -282,7 +282,7 @@ if ($result->num_rows > 0) {
                     href="../frontend/payment.html"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                    Read more
+                <?php echo $row['barbieprice']; ?>
                     <svg
                         class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
                         aria-hidden="true"
